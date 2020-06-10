dceb93c7cdedede8a27b00b8170d0a597b23e8a6
Anjelika <lika.bogucharova@mail.ru>
2020-06-10 22:42:10 +0300

    Initial commit

---
+++ src/main/java/com/anjelika/MainClass.java
@@ -0,0 +1,22 @@
+package com.anjelika;
+
+import com.anjelika.personConfig.Person;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Person p1 = new Person(true, "Валентин");
+        Person p2 = new Person(false, "Александра");
+        Person p3 = new Person(true, "Евгений");
+        Person p4 = new Person(false, "Мария");
+        Person p5 = new Person(true, "Петр");
+        Person p6 = new Person(false, "Земфира");
+
+        p1.marry(p2);
+        p3.marry(p4);
+        p6.divorce();
+        p6.marry(p4);
+        p6.marry(p1);
+        p2.marry(p5);
+        p2.divorce();
+    }
+}
